8ad9ddaebddd9a18719044ea749507d092c32651
pilli <elpilli@gmail.com>
2014-03-31 13:38:01 +0200

    Movie attributes added. Development data added.

--- server/grails-app/conf/BootStrap.groovy
+++ server/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,21 @@
+import java.text.DateFormat;
+
+import es.pilli.rotten_potatoes.Movie
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+		new Movie(title: "Aladdin", rating: "G", releaseDate: new Date().parse("dd-MM-yyyy", "25-11-1992")).save(failOnError:true, flush:true);
+		new Movie(title: "The Terminator", rating: "R", releaseDate: new Date().parse("dd-MM-yyyy", "26-10-1984")).save(failOnError:true, flush:true);
+		new Movie(title: "When Harry Met Sally", rating: "R", releaseDate: new Date().parse("dd-MM-yyyy", "21-07-1989")).save(failOnError:true, flush:true);
+		new Movie(title: "The Help", rating: "PG-13", releaseDate: new Date().parse("dd-MM-yyyy", "10-08-2011")).save(failOnError:true, flush:true);
+		new Movie(title: "Chocolat", rating: "PG-13", releaseDate: new Date().parse("dd-MM-yyyy", "5-01-2001")).save(failOnError:true, flush:true);
+		new Movie(title: "Amelie", rating: "R", releaseDate: new Date().parse("dd-MM-yyyy", "25-04-2001")).save(failOnError:true, flush:true);
+		new Movie(title: "2001: A Space Odyssey", rating: "G", releaseDate: new Date().parse("dd-MM-yyyy", "6-04-1968")).save(failOnError:true, flush:true);
+		new Movie(title: "The Incredibles", rating: "PG", releaseDate: new Date().parse("dd-MM-yyyy", "5-11-2004")).save(failOnError:true, flush:true);
+		new Movie(title: "Raiders of the Lost Ark", rating: "PG", releaseDate: new Date().parse("dd-MM-yyyy", "12-06-1981")).save(failOnError:true, flush:true);
+		new Movie(title: "Chicken Run", rating: "G", releaseDate: new Date().parse("dd-MM-yyyy", "21-06-2000")).save(failOnError:true, flush:true);
+	}
+	def destroy = {
+	}
 }
--- server/grails-app/domain/es/pilli/rotten_potatoes/Movie.groovy
+++ server/grails-app/domain/es/pilli/rotten_potatoes/Movie.groovy
@@ -2,6 +2,15 @@ package es.pilli.rotten_potatoes
 
 class Movie {
 
+	String title;
+	String rating;
+	Date releaseDate;
+	String description;
+	
     static constraints = {
+		title (blank:false);
+		rating (blank:false, inList: ["G", "PG", "PG-13", "R"]);
+		releaseDate (blank:false);
+		description (nullable:true);
     }
 }
